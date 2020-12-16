7c5ea5c6c4a8b106716f6f31e1c21ba6edd8e645
Jose Medina <josemedina921@gmail.com>
2020-12-15 22:16:12 -0800

    updates

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyApp() {
+    return <div>This is my app</div>;
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("app"));
